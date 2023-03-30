29e7f113e351c2a823a2d8371018cec2174fc6a1
Marcos Tenorio <marcosvtenoriodev@gmail.com>
2023-03-30 17:52:04 -0300

    :lipstick: feat: Estilizaçao CSS da atividade

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ let clearBtn = document.getElementById('clearBtn');
 
 const dado = () =>{
     let dadoResult = Math.floor(Math.random() * 6)+1;
-    result.innerHTML += `<h1>o seu dado caiu com o resultado ${dadoResult}</h1>`
+    result.innerHTML += `<h3>o seu dado caiu com o resultado ${dadoResult}</h3>`
+    result.innerHTML += `<h3>-------------------------------------------------</h3>`
     console.log(Math.floor(Math.random() * 6));
 }
 button.onclick = dado;
 
 const clear = () =>{
-    result.innerHTML = `<h2>O seu resultado será inserido em seguida...</h2>`
+    result.innerHTML = `<h2>O seu resultado será inserido em seguida...<br><br></h2>`
 }
 clearBtn.onclick = clear;
