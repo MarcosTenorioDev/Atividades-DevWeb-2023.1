4e738bf0b0d63340385230d28b849c2a5d95d435
MarcosTenorio <marcosvtenoriodev@gmail.com>
2023-03-31 11:41:05 -0300

    :bulb: docs: Comentários para melhor entendimento do código

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
+// Declaração de variáveis
 let button = document.getElementById('button');
 let result = document.getElementById('result');
 let clearBtn = document.getElementById('clearBtn');
 
-
+//Função para obter o valor do resultado do dado
 const dado = () =>{
     let dadoResult = Math.floor(Math.random() * 6)+1;
     result.innerHTML += `<h3>o seu dado caiu com o resultado ${dadoResult}</h3>`
     result.innerHTML += `<h3>-------------------------------------------------</h3>`
-    console.log(Math.floor(Math.random() * 6));
 }
 button.onclick = dado;
 
+//Função para limpar
 const clear = () =>{
-    result.innerHTML = `<h2>O seu resultado será inserido em seguida...<br><br></h2>`
+    result.innerHTML = `<h4>O seu resultado será inserido em seguida...<br><br></h4>`
 }
 clearBtn.onclick = clear;
